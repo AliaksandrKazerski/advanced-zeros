9783ac99a69a9119aa2737c8a5506fa41e92d140
Aliaksandr Kazerski <sashfob@mail.ru>
2019-02-27 21:43:43 +0300

    add function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var prNum = [2], resultArr = [];
+  next:// create an array of primes range from 0 to base
+  for (var i = 2; i <= base; i++) {
+    for (var j = 2; j < i; j++) {
+      if (i % j === 0) {
+        continue next;
+      }
+      if (i !== prNum[prNum.length - 1])
+      prNum.push(i);
+    } 
+  }
+  for (i = 0; i < prNum.length; i++) { // loop divides base into prime numbers and divides number into this prime numbers
+    if (base === 1) { 
+      break;
+    }
+    var count = 0;
+    var result = 0;
+    var numLoop = number;
+    while (base % prNum[i] === 0){ 
+      count++; // save pow
+      base /= prNum[i];
+    }
+    while (numLoop >= prNum[i]) {
+      numLoop = Math.floor(numLoop / prNum[i]);
+      result += numLoop;
+    }
+    resultArr.push(Math.floor(result / count));   //  pow needed here and result push on array
+  }
+  return Math.min(...resultArr); // return min result from array
 }
